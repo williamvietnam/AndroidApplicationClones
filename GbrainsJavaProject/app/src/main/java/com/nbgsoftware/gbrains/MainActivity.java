ba47b03fee099efdcd54751f3562025a41825d87
William <williamnb.vn@gmail.com>
2022-05-29 15:21:24 +0700

    JAVA-01: First commit branch dev debug java

--- GbrainsJavaProject/app/src/main/java/com/nbgsoftware/gbrains/MainActivity.java
+++ GbrainsJavaProject/app/src/main/java/com/nbgsoftware/gbrains/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.nbgsoftware.gbrains.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.textView.setText("This is branch dev debug with java programing");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
     }
 }
