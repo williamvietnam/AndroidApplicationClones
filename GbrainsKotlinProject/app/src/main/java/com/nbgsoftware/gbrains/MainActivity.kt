266701b27f4d7021aa8b0e16e659adc8defbea10
William <williamnb.vn@gmail.com>
2022-05-29 15:25:50 +0700

    KOTLIN-01: First commit branch dev debug kotlin

--- GbrainsKotlinProject/app/src/main/java/com/nbgsoftware/gbrains/MainActivity.kt
+++ GbrainsKotlinProject/app/src/main/java/com/nbgsoftware/gbrains/MainActivity.kt
@@ -2,10 +2,20 @@ package com.nbgsoftware.gbrains
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nbgsoftware.gbrains.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.textView.text = "This is branch Kotlin project"
     }
 }
