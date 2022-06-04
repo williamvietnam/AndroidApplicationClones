c9c95ccb3678aea484ffdc556e5ac4bbe128e670
William <williamnb.vn@gmail.com>
2022-06-04 11:08:36 +0700

    Xử lý xong logic màn welcome

--- GbrainsJavaProject/app/src/main/java/com/nbgsoftware/gbrains/ui/welcome/WelcomeFragment.java
+++ GbrainsJavaProject/app/src/main/java/com/nbgsoftware/gbrains/ui/welcome/WelcomeFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.nbgsoftware.gbrains.R;
 import com.nbgsoftware.gbrains.adapters.SlideWelcomeAdapter;
 import com.nbgsoftware.gbrains.baseMVP.MVPFragment;
 import com.nbgsoftware.gbrains.data.models.response.WelcomeResponse;
@@ -19,13 +20,8 @@ import com.nbgsoftware.gbrains.databinding.FragmentWelcomeBinding;
 public class WelcomeFragment extends MVPFragment<FragmentWelcomeBinding> implements WelcomeContract.View {
 
     WelcomeContract.Presenter<WelcomeContract.View> presenter = new WelcomePresenter<>();
-    private Handler handler = new Handler();
-    private Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-
-        }
-    };
+    private final Handler handler = new Handler();
+    private Runnable runnable;
 
     @Override
     public FragmentWelcomeBinding createViewBinding(LayoutInflater inflater, ViewGroup container) {
@@ -43,6 +39,7 @@ public class WelcomeFragment extends MVPFragment<FragmentWelcomeBinding> impleme
         super.onViewCreated(view, savedInstanceState);
         presenter.onViewCreated();
         presenter.getData(requireContext());
+        binding.btnStarted.setOnClickListener(view1 -> findNavController().navigate(R.id.actionToSignIn));
     }
 
     @Override
@@ -52,16 +49,43 @@ public class WelcomeFragment extends MVPFragment<FragmentWelcomeBinding> impleme
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, 2000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+    }
+
+    @Override
     public void showData(WelcomeResponse response) {
         SlideWelcomeAdapter adapter = new SlideWelcomeAdapter(response, requireContext());
         binding.viewPager2.setAdapter(adapter);
         binding.indicator3.setViewPager(binding.viewPager2);
-
+        binding.btnContinue.setVisibility(View.VISIBLE);
+        binding.btnContinue.setOnClickListener(view -> {
+            if (binding.viewPager2.getCurrentItem() == response.getWelcomeList().size() - 1) {
+                binding.viewPager2.setCurrentItem(0);
+            } else {
+                binding.viewPager2.setCurrentItem(binding.viewPager2.getCurrentItem() + 1);
+            }
+        });
+        runnable = () -> {
+            if (binding.viewPager2.getCurrentItem() == response.getWelcomeList().size() - 1) {
+                binding.viewPager2.setCurrentItem(0);
+            } else {
+                binding.viewPager2.setCurrentItem(binding.viewPager2.getCurrentItem() + 1);
+            }
+        };
         binding.viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
-
+                handler.removeCallbacks(runnable);
+                handler.postDelayed(runnable, 2000);
             }
         });
     }
