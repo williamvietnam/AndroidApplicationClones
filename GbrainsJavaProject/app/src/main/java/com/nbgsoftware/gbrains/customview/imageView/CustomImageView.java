37bc3452a56bad4ab06a57deb8729830465fa779
William <williamnb.vn@gmail.com>
2022-06-12 17:48:29 +0700

    Customview layout for: button, imageview, edittext, textview

---
+++ GbrainsJavaProject/app/src/main/java/com/nbgsoftware/gbrains/customview/imageView/CustomImageView.java
@@ -0,0 +1,30 @@
+package com.nbgsoftware.gbrains.customview.imageView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.nbgsoftware.gbrains.databinding.LayoutCustomImageViewBinding;
+
+public class CustomImageView extends ConstraintLayout {
+    private LayoutCustomImageViewBinding binding;
+
+    public CustomImageView(@NonNull Context context) {
+        super(context);
+    }
+
+    public CustomImageView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomImageView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomImageView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
---
+++ GbrainsJavaProject/app/src/main/java/com/nbgsoftware/gbrains/customview/textView/CustomTextView.java
@@ -0,0 +1,30 @@
+package com.nbgsoftware.gbrains.customview.textView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.nbgsoftware.gbrains.databinding.LayoutCustomTextViewBinding;
+
+public class CustomTextView extends ConstraintLayout {
+    private LayoutCustomTextViewBinding binding;
+
+    public CustomTextView(@NonNull Context context) {
+        super(context);
+    }
+
+    public CustomTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
