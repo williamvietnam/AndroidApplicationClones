6e9d3e4e23df4924512108e29c54215534400bcd
William <williamnb.vn@gmail.com>
2022-06-21 21:20:35 +0700

    Jetpack Compose

---
+++ GbrainsComposeProject/app/src/main/java/com/nbgsoftware/gbrains/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.nbgsoftware.gbrains
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nbgsoftware.gbrains.ui.theme.GbrainsComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GbrainsComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GbrainsComposeTheme {
+        Greeting("Android")
+    }
+}
