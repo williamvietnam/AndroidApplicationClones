c4f70e0c30b7c299857700d52bce8a133df03228
NguyenBangGiang <williamnb.vn@gmail.com>
2023-02-17 16:50:58 +0700

    ins_clone: create codebase MVVM Kotlin

---
+++ InstagramClone/app/src/main/java/base/BaseViewHolder.kt
@@ -0,0 +1,34 @@
+package com.base.mvvm.core.base
+
+import android.view.View
+
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private var currentPosition = 0
+
+    open fun onBind(position: Int) {
+        this.currentPosition = position
+        clear()
+    }
+
+    protected abstract fun clear()
+
+    fun getCurrentPosition(): Int {
+        return this.currentPosition
+    }
+}
+
+abstract class BaseViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+
+    var currentPosition = 0
+        private set
+
+    abstract fun clear()
+
+    open fun onBind(position: Int) {
+        currentPosition = position
+        clear()
+    }
+
+}
---
+++ InstagramClone/app/src/main/java/data/network/AuthenticationApiServices.kt
@@ -0,0 +1,8 @@
+package com.base.mvvm.core.data.network
+
+/**
+ * Author: William Giang Nguyen | 8/7/2022
+ * */
+interface AuthenticationApiServices{
+
+}
