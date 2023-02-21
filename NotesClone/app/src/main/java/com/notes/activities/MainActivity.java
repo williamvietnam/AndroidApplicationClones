68e652ec9da17ed0a108128e4fe2cc8890de047a
NguyenBangGiang <williamnb.vn@gmail.com>
2023-02-21 23:17:43 +0700

    notes_clone: create note activity complete

--- NotesClone/app/src/main/java/com/notes/activities/MainActivity.java
+++ NotesClone/app/src/main/java/com/notes/activities/MainActivity.java
@@ -1,16 +1,37 @@
 package com.notes.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.notes.R;
+import com.notes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.binding.btnAddNotesMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
     }
 }
