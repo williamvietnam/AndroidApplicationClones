0c03d5b52a72d91558f6b21626eb74d20c1dcb0b
NguyenBangGiang <williamnb.vn@gmail.com>
2023-02-21 22:41:27 +0700

    notes_clone: create layout notes management

---
+++ NotesClone/app/src/main/java/com/notes/activities/CreateNoteActivity.java
@@ -0,0 +1,6 @@
+package com.notes.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateNoteActivity extends AppCompatActivity {
+}
--- NotesClone/app/src/main/java/com/notes/activities/MainActivity.java
+++ NotesClone/app/src/main/java/com/notes/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.notes;
+package com.notes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.notes.R;
 
 public class MainActivity extends AppCompatActivity {
 
